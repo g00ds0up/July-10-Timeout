cb633037d64030e9c609f784a11a510648b909a5
Jayce Avery <Jred032@yahoo.com>
2023-07-10 19:15:59 -0400

    01 done

--- problems/01-set-seconds-timeout.js
+++ problems/01-set-seconds-timeout.js
@@ -24,8 +24,12 @@ setSecondsTimeout(function () {
 ***********************************************************************/
 
 function setSecondsTimeout(cb, delayInSeconds) {
-  // Your code here
+  return setTimeout(cb, delayInSeconds * 1000)
 }
+
+setSecondsTimeout(function () {
+  console.log('hello');
+}, 1); // should print 'hello' after 1000 milliseconds
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
